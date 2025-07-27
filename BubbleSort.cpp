3f46076bd28601199d180f3634e8195b5dd5c30e
ostuxcat <ameya992004@gmail.com>
2025-07-28 00:34:21 +0530

    BubbleSort Optimised For Better Time Complexity

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,8 +4,10 @@ using namespace std;
 void BubbleSort(vector<int> &arr){
     int n = arr.size()-1;
     for(int i=0;i<n;i++){
+        bool isSwaped = false;
         for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1]) swap(arr[j], arr[j+1]);
+            if(arr[j]>arr[j+1]) swap(arr[j], arr[j+1]); isSwaped=true;
         }
+        if(!isSwaped) return;
     }
 }
