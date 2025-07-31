3e4eb4c54987730ec01635869d8b765bbc3bfda1
ostuxcat <ameya992004@gmail.com>
2025-07-31 19:58:28 +0530

    Clean UP

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 // int main(){
 //     vector<int> arr = {4, 1, 5, 2, 3};
 //     BubbleSort(arr);
-//     for(int i=0;i<arr.size();i++){
-//         cout << arr[i] << " ";
+//     for(int val:arr){
+//         cout << val << " ";
 //     }
 //     cout << endl;
 //     return 0;
@@ -21,8 +21,8 @@ using namespace std;
 // int main(){
 //     vector<int> arr = {4, 1, 5, 2, 3};
 //     SelectionSort(arr);
-//     for(int i=0;i<arr.size();i++){
-//         cout << arr[i] << " ";
+//     for(int val:arr){
+//         cout << val << " ";
 //     }
 //     cout << endl;
 //     return 0;
@@ -34,8 +34,8 @@ using namespace std;
 // int main(){
 //     vector<int> arr = {4, 1, 5, 2, 3};
 //     InsertionSort(arr);
-//     for(int i=0;i<arr.size();i++){
-//         cout << arr[i] << " ";
+//     for(int val:arr){
+//         cout << val << " ";
 //     }
 //     cout << endl;
 //     return 0;
@@ -47,8 +47,8 @@ using namespace std;
 // int main(){
 //     vector<int> arr = {2, 0, 2, 1, 1, 0, 1, 2, 0, 0};
 //     DutchSort(arr);
-//     for(int i=0;i<arr.size();i++){
-//         cout << arr[i] << " ";
+//     for(int val:arr){
+//         cout << val << " ";
 //     }
 //     cout << endl;
 //     return 0;
@@ -62,26 +62,26 @@ using namespace std;
 //     vector<int> arr2 = {2, 5, 6};
 //     int m=3, n=3;
 //     MergeSortedArray(arr1, m, arr2, n);
-//     for(int i=0;i<arr1.size();i++){
-//         cout << arr1[i] << " ";
+//     for(int val:arr1){
+//         cout << val << " ";
 //     }
 //     cout << endl;
-//     for(int i=0;i<arr2.size();i++){
-//         cout << arr2[i] << " ";
+//     for(int val:arr2){
+//         cout << val << " ";
 //     }
 //     cout << endl;
 //     return 0;
 // }
 //
 // NEXT PERMUTATION
-#include<vector>
-#include "./Modules/NextPermutation.cpp"
-int main(){
-    vector<int> arr = {1, 2, 3};
-    NextPermutation(arr);
-    for(int i=0;i<arr.size();i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    return 0;
-}
+// #include<vector>
+// #include "./Modules/NextPermutation.cpp"
+// int main(){
+//     vector<int> arr = {1, 2, 3};
+//     NextPermutation(arr);
+//     for(int val:arr){
+//         cout << val << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
